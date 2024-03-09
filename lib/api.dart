0357b20e49ac5994af2789b799e836157a632fe1
DylanBozarth <dylanabozarth@gmail.com>
2024-03-09 14:00:42 -0800

    partial match

--- lib/api.dart
+++ lib/api.dart
@@ -49,26 +49,38 @@ makeGetRequest(barcode, lookingForThings) async {
 
 void findThingsInIngredients(
     List<Map<String, dynamic>> filteredResults, List<String> lookingForThings) {
-  List<String> desiredStrings = lookingForThings.toList();
+  List<String> desiredStrings = lookingForThings.map((s) => s.toLowerCase()).toList();
   List<String> matchedIngredients = [];
 
-  if (desiredStrings.contains("Seed Oils")) {
-    desiredStrings.addAll(seedOils);
-    // print('Desired Strings $desiredStrings');
+  if (desiredStrings.contains("seed oils")) {
+    desiredStrings.addAll(seedOils.map((s) => s.toLowerCase()));
+  } else if (desiredStrings.contains("added sugar")) {
+    desiredStrings.addAll(addedSugar.map((s) => s.toLowerCase()));
   }
-  print(filteredResults);
-  print(desiredStrings); // both are now lower case, now compare them
-  Set<String> set1 = desiredStrings.toSet();
-  Set<Map<String, dynamic>> set2 = filteredResults.toSet();
 
-  Set<Map<String, dynamic>> commonElements = set2.intersection(set1);
+  print('filtered results $filteredResults');
+  print('desired Strings: $desiredStrings');
+
+  // Print cleaned-up strings from filteredResults
+  List<String> cleanedUpStrings = filteredResults
+      .map((entry) => entry["key"].toString().toLowerCase())
+      .map((s) => s.replaceAll(RegExp(r'[^\w\s]'), ' '))
+      .toList();
+  print('Cleaned-up Strings: $cleanedUpStrings');
+
+  // Split the cleaned-up strings into individual words
+  Set<String> set1 = desiredStrings.toSet();
+  Set<String> set2 = cleanedUpStrings
+      .expand((s) => s.split(' '))
+      .map((s) => s.trim()) // Remove leading/trailing spaces
+      .where((s) => s.isNotEmpty) // Remove empty strings
+      .toSet();
+
+  Set<String> commonElements = set2.intersection(set1);
 
   print('Common elements: $commonElements');
   // print('Matched ingredients: $matchedIngredients');
 
-  //Dump everything for next scan   known error: wipes the looking for array
   desiredStrings.clear();
   matchedIngredients.clear();
 }
-
-//image: image_url
