b6b2bd55aec26a12259550872be82da50462dc94
DylanBozarth <dylanabozarth@gmail.com>
2025-04-27 18:51:40 -0700

    slug-name

---
+++ app.config.js
@@ -0,0 +1,80 @@
+export default {
+  expo: {
+    name: "whatsinmyfood",
+    slug: "what-is-in-my-food",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSCameraUsageDescription: "$(PRODUCT_NAME) needs access to your Camera.",
+        ITSAppUsesNonExemptEncryption: false
+      },
+      bundleIdentifier: "com.dylanbozarthwork.whatsinmyfood"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      permissions: [
+        "android.permission.CAMERA",
+        "android.permission.RECORD_AUDIO"
+      ],
+      package: "com.dylanbozarthwork.whatsinmyfood"
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ],
+      [
+        "expo-camera",
+        {
+          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
+          microphonePermission: "Allow $(PRODUCT_NAME) to access your microphone",
+          recordAudioAndroid: true
+        }
+      ],
+      [
+        "expo-build-properties",
+        {
+          android: {
+            kotlinVersion: "1.9.25",
+            gradleProperties: {
+              kotlinVersion: "1.9.25",
+              composeCompilerVersion: "1.5.15"
+            }
+          }
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      router: {
+        origin: false
+      },
+      eas: {
+        projectId: "37b5ee22-07d2-4c00-a69d-c46f56e723e0"
+      }
+    },
+    owner: "dylanbozarthdev"
+  }
+};
