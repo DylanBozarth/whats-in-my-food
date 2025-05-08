f84b2a3f20e3fa39bb8903c2fbc19f8dd852732e
DylanBozarth <dylanbozarth@theluxergroup.com>
2025-05-08 14:37:32 -0700

    todo

--- pages/todo.js
+++ pages/todo.js
@@ -2,7 +2,7 @@
 Add error handling to all the network request
 Add description for each ingredient
 handle non-english results (maybe) 
-
+show proper screen when something has not been scanned 
 
 
 */
